b478ff37093b595d8a8840b1d7f1f57ab5835c9d
Gustavo Cesarino <gccesarino220@gmail.com>
2023-09-07 23:50:50 -0300

    barra aumenta e diminui segurando o botao

--- logic.js
+++ logic.js
@@ -1,30 +1,48 @@
 let btAccel = document.getElementById('btAccel')
-btAccel.addEventListener('click', acceleration)
+//btAccel.addEventListener('click', acceleration)
 
 let element = document.querySelector('#accelBarInside')
-let style = getComputedStyle(element);
-document.addEventListener("DOMContentLoaded", function() {
-    
+let style = getComputedStyle(element); // mudar o nome dessas variaveis
+
+document.addEventListener("DOMContentLoaded", function (){
     function forceDepletion(){
         let largura = element.style.width;
         let larguraAsInt = parseInt(largura, 10);
-        if(larguraAsInt > 0){
+        if (larguraAsInt - 10 >= 1)
+        {
             let decrementLargura = larguraAsInt - 10;
             element.style.width = decrementLargura + "px";
+            
+        }else{
+            larguraAsInt = 1;
         }
-        
-        setTimeout(forceDepletion,1000);
+        console.log("Largura: "+larguraAsInt+"px");
+        setTimeout(forceDepletion, 50);
     }
-    
     forceDepletion();
+});
+
+//btAccel.addEventListener('click', acceleration);
+let intervalId;
+
+btAccel.addEventListener('mousedown', function () {
+    clearInterval(intervalId);
+    intervalId = setInterval(acceleration, 50);
+    console.log(intervalId);
+});
+
+btAccel.addEventListener('mouseup', function () {
+    clearInterval(intervalId);
+    console.log("Interval cleared!");
 });
 
 function acceleration(){
     let largura = style.width;
     let larguraAsInt = parseInt(largura, 10);
-    let incrementedLargura = larguraAsInt + 20;
-    //alert(incrementedWidth);
+    let incrementedLargura = larguraAsInt + 40;
+    //alert(incrementedLargura);
+    //console.log("largura"+incrementedLargura);
     element.style.width = incrementedLargura + "px";
-
 }
+
 
